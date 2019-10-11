8f62301cf6c3cf753c527a7fc98d0b9a2601c03b
im_gkl <sendittogokul@gmail.com>
2019-10-11 20:36:32 +0530

    implemented tinder-like cards

--- lib/movieList.dart
+++ lib/movieList.dart
@@ -1,6 +1,6 @@
-import 'dart:ui' as ui;
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
 
 class MovieBuilder extends StatefulWidget {
   @override
@@ -13,64 +13,51 @@ class _MovieBuilderState extends State<MovieBuilder> {
     initialPage: 0,
   );
   @override
-  Widget build(BuildContext context) {
+  Widget build(
+    BuildContext context,
+  ) {
     return StreamBuilder<QuerySnapshot>(
       stream: Firestore.instance.collection('movies').snapshots(),
       builder: (context, snapshot) {
         if (!snapshot.hasData) {
           const Text("Loading");
         } else {
-          return PageView.builder(
-              pageSnapping: true,
-              physics: BouncingScrollPhysics(),
-              controller: controller,
-              itemCount: snapshot.data.documents.length,
-              itemBuilder: (context, index) {
-                DocumentSnapshot mypost = snapshot.data.documents[index];
-                return Stack(
-                  children: <Widget>[
-                    Image.network(
-                      "${mypost['bg']}",
-                      fit: BoxFit.cover,
-                      filterQuality: FilterQuality.low,
-                      height: double.infinity,
-                    ),
-                    ClipRect(
-                      child: BackdropFilter(
-                        filter: ui.ImageFilter.blur(sigmaX: 20, sigmaY: 20),
-                        child: Center(
-                          child: Container(
-                            height: 510,
-                            child: Card(
-                              color: Colors.black,
-                              child: Column(
-                                children: <Widget>[
-                                  Container(
-                                    child: Image.network(
-                                      "${mypost['bg']}",
-                                      fit: BoxFit.values[2],
-                                    ),
-                                    height: 450,
-                                  ),
-                                  Padding(
-                                    padding: const EdgeInsets.only(top: 8.0),
-                                    child: Text(
-                                      '${mypost['title']}',
-                                      style: TextStyle(
-                                          fontSize: 30,
-                                          fontWeight: FontWeight.bold),
-                                    ),
-                                  ),
-                                ],
+          return Stack(
+            children: <Widget>[
+              Swiper(
+                loop: true,
+                index: 0,
+                itemCount: snapshot.data.documents.length,
+                layout: SwiperLayout.TINDER,
+                itemWidth: 500,
+                itemHeight: 708,
+                itemBuilder: (BuildContext context, index) {
+                  DocumentSnapshot mypost = snapshot.data.documents[index];
+                  return Stack(
+                    children: <Widget>[
+                      Center(
+                        child: Card(
+                          color: Colors.black,
+                          child: Column(
+                            children: <Widget>[
+                              Image.network(
+                                "${mypost['bg']}",
+                                fit: BoxFit.cover,
+                                width: 500,
+                                height: 600,
                               ),
-                            ),
+                              Text("${mypost['title']}")
+                            ],
                           ),
                         ),
                       ),
-                    ),
-                  ],
-                );
-              });
+                    ],
+                  );
+                  // return Center(child: Text("Hello"));
+                },
+              ),
+            ],
+          );
         }
         return CircularProgressIndicator();
       },
